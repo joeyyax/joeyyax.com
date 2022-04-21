7f866b968d7f0b4d810382947826c3c39e78c84e
Joey Yax <joey@joeyyax.com>
2022-04-21 15:49:51 -0700

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { useRouter } from "next/router";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
@@ -21,6 +22,13 @@ function App({ Component, pageProps }) {
 
   return (
     <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`}
+      </Script>
       <Navbar />
       <AnimatePresence exitBeforeEnter>
         <LazyMotion features={domAnimation}>
