9d29f2ce627b15ec7d8580b5ecba1fd3d39fd117
Joey Yax <joey@joeyyax.com>
2022-04-22 16:02:45 -0700

    placeholders for favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic"
+import Head from "next/head"
 import GoogleTagManager from "lib/GoogleTagManager"
 import PageTransitionWrapper from "components/PageTransitionWrapper/PageTransitionWrapper"
 
@@ -10,6 +11,29 @@ import "../styles/globals.css"
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" /> */}
+      </Head>
       <GoogleTagManager.Script />
       <Navbar />
       <PageTransitionWrapper>
