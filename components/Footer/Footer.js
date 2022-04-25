8d94389e280ace3c20858b657d6ea8906e03dab0
Joey Yax <joey@joeyyax.com>
2022-04-25 16:27:47 -0700

    Convert privacy policy to a disclaimer, footer organization

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,14 +7,10 @@ const Footer = () => {
 
   return (
     <Section type="footer" name="footer" className="">
-      <Section.Container className="flex-col lg:flex-row lg:py-8 text-slate-500 dark:text-slate-500 justify-between">
+      <Section.Container className="flex-col lg:flex-row lg:py-8 text-slate-500 dark:text-slate-300 justify-between">
         <div className="legal text-xs leading-relaxed">
           Contents of this website are &copy; {currentYear} Joey Yax. All rights
           reserved.
-          {` `}
-          <Link href="/privacy-policy">
-            <a>Privacy Policy</a>
-          </Link>
           <br />
           <a
             href="https://github.com/joeyyax/joeyyax.com"
@@ -32,6 +28,10 @@ const Footer = () => {
             MIT license
           </a>
           .
+          <br />
+          <Link href="/disclaimer">
+            <a>Disclaimer</a>
+          </Link>
         </div>
         <Socials />
       </Section.Container>
