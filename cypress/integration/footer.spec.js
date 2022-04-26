ca3b1c1698c03eee0cdea57f2ee4401310cc7096
Joey Yax <joey@joeyyax.com>
2022-04-26 15:38:42 -0700

    update e2e tests

---
+++ cypress/integration/footer.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("Footer", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("Disclaimer link is present", () => {
+    cy.get('footer a[href*="/disclaimer"]')
+  })
+})
