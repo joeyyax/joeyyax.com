b4839b4869911183f0df765b5bf7e9e2ae4d1500
Joey Yax <joey@joeyyax.com>
2022-04-25 17:17:12 -0700

    track brand clicks

--- components/Branding/Branding.js
+++ components/Branding/Branding.js
@@ -5,7 +5,13 @@ import classNames from "classnames"
 const Branding = () => {
   return (
     <Link href="/" passHref>
-      <a className="branding inline-flex font-bold uppercase text-slate-400 hover:text-slate-500 dark:text-slate-600 border-2 border-slate-300  dark:border-slate-800 px-2 rounded-md">
+      <a
+        className={classNames(
+          "branding",
+          "inline-flex font-bold uppercase text-slate-400 hover:text-slate-500 dark:text-slate-600 border-2 border-slate-300  dark:border-slate-800 px-2 rounded-md",
+          "umami--click--branding"
+        )}
+      >
         Joey Yax
       </a>
     </Link>
